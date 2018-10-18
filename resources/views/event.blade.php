b6b108fe56a04025ada9c995bdc6e28a566e8dfc
Artem2710 <you@example.com>
2018-10-18 12:01:35 +0300

    connect to event and select by type

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -5,11 +5,12 @@
     <div class="container about-event">
         <p>Title: {{$event->title}}</p>
         <p>Description: {{$event->description}}</p>
-        <p>Participants:
+        <p>Time spending: {{$event->dateTime}}</p>
+        <p>Participants: </p>
         @foreach($names as $name)
-            {{ $name }};
+            <p style="margin-left: 20px">{{ $name }}</p>
         @endforeach
-        </p>
+
         <div class="banner-btn">
             <a href="{{route('events.index')}}">show events</a>
         </div>
@@ -17,6 +18,32 @@
         @if(Auth::id() == $event->user_id)
             <a href="{{route('events.edit', ['id' => $event->id])}}">edit</a>
         @endif
+        @auth
+            {{--@if($participant->user_id == Auth::id())--}}
+            <form method="post" action="{{route('participate.delete', ['event' => $event, 'names' => $names,])}}">
+                {{ method_field('delete') }}
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <div>
+                    <button name="event_id" value="">leave</button>
+                </div>
+            </form>
+            {{--@else--}}
+        @endauth
+        @auth
+
+            <form method="post" action="{{route('participate', ['event' => $event, 'names' => $names,])}}">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <div>
+                    <button name="event_id" value="{{$event->id}}">connect</button>
+                </div>
+                @if ($errors->has('event'))
+                    <span class="help-block">
+                            <strong>{{ $errors->first('event') }}</strong>
+                        </span>
+                @endif
+            </form>
+            {{--@endif--}}
+        @endauth
     </div>
 
 @endsection
